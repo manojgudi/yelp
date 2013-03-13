e06059270b35e9bcd71d21ae5ee99089f7488434
manojgudi <manoj.p.gudi@gmail.com>
2013-03-13 23:03:48 +0530

    added correct try block to suppress common error

--- function_testing/return_line_file.py
+++ function_testing/return_line_file.py
@@ -4,27 +4,28 @@ def return_line_no(keyword_list,file_list):
 		import os
 		path = (os.getcwd())
 
-		#
-		file_obj = open(path+'/'+file_name,'r')
-		file_text = file_obj.read()
-		file_obj.close()
-		#file_text = rm_comments(file_text) # file_text should be commentless
+		try:
 		
-		# Iterate for keywords
-		for keyword in keyword_list:
+			file_obj = open(path+'/'+file_name,'r')
+			file_text = file_obj.read()
+			file_obj.close()
+			#file_text = rm_comments(file_text) # file_text should be commentless
 		
-			if file_text.count(keyword) != 0:
-				file_text_list = file_text.splitlines()
-				line_no = 0
-				for line in file_text_list:
-					line_no += 1
-					if line.count(keyword) != 0:
-						file_keyword_list.append([line_no, file_name])
-			else:
-				pass
+			# Iterate for keywords
+			for keyword in keyword_list:
 			
-		#except:
-		#print("File " + file_name + " not found")
+				if file_text.count(keyword) != 0:
+					file_text_list = file_text.splitlines()
+					line_no = 0
+					for line in file_text_list:
+						line_no += 1
+						if line.count(keyword) != 0:
+							file_keyword_list.append([line_no, file_name])
+				else:
+					pass
+			
+		except:
+			print("File " + file_name + " not found")
 		
 	return file_keyword_list
 		
